34b5af46e51958b62c1f20ca3305e89d58a8a342
mahibashardeveloper <mahibashar2023@gmail.com>
2023-09-19 14:58:29 +0600

    service and footer update

---
+++ widgets/service-2.php
@@ -0,0 +1,63 @@
+<div class="service-2-section pb-5">
+    <div class="h1 text-white text-center pt-5 pb-5">
+        Title Here
+    </div>
+
+    <div class="container py-5">
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-2">
+            <div class="px-2 pb-4">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="assets/images/pawprint.png" class="img-fluid" alt="pawprint">
+                    </div>
+                    <div class="col-9">
+                        <div class="h2 text-white">Lorem ipsum dolor.</div>
+                        <div class="lead fw-bold text-white">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda dolorem explicabo, facilis odio qui voluptatibus!
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="px-2 pb-4">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="assets/images/pawprint.png" class="img-fluid" alt="pawprint">
+                    </div>
+                    <div class="col-9">
+                        <div class="h2 text-white">Lorem ipsum dolor.</div>
+                        <div class="lead fw-bold text-white">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda dolorem explicabo, facilis odio qui voluptatibus!
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="px-2 pb-4">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="assets/images/pawprint.png" class="img-fluid" alt="pawprint">
+                    </div>
+                    <div class="col-9">
+                        <div class="h2 text-white">Lorem ipsum dolor.</div>
+                        <div class="lead fw-bold text-white">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda dolorem explicabo, facilis odio qui voluptatibus!
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="px-2 pb-4">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="assets/images/pawprint.png" class="img-fluid" alt="pawprint">
+                    </div>
+                    <div class="col-9">
+                        <div class="h2 text-white">Lorem ipsum dolor.</div>
+                        <div class="lead fw-bold text-white">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda dolorem explicabo, facilis odio qui voluptatibus!
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
