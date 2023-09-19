c34a47f95669a81e4d27714b4df6ab6ea8d0e7ef
Noyon Ahammed Sojon <ahammedsojon000@gmail.com>
2023-09-19 18:55:41 +0600

    script tags replaced to script file

--- widgets/scripts.php
+++ widgets/scripts.php
@@ -1,2 +1,4 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/js/bootstrap.bundle.min.js"></script>
 <script src="./assets/js/main.js"></script>
