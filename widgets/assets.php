44b0617a73caee36bec1b89be272373ae815f146
mahibashardeveloper <mahibashar2023@gmail.com>
2023-09-19 12:51:31 +0600

    template setup

--- widgets/assets.php
+++ widgets/assets.php
@@ -1,3 +1,3 @@
-<link rel="stylesheet" href="assets/bootstrap/dist/css/bootstrap.min.css">
-<link rel="stylesheet" href="assets/bootstrap-icons/font/bootstrap-icons.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.11.1/font/bootstrap-icons.min.css"/>
 <link rel="stylesheet" href="assets/css/style.css">
--- widgets/scripts.php
+++ widgets/scripts.php
@@ -1,1 +1,1 @@
-<script src="assets/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/js/bootstrap.bundle.min.js"></script>
