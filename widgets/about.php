cbceea82bb5fe420c75b729fea34c954cf3e9d42
Noyon Ahammed Sojon <ahammedsojon000@gmail.com>
2023-09-19 17:31:48 +0600

    about section ui completed

---
+++ widgets/about.php
@@ -0,0 +1,49 @@
+<section class="about-section">
+    <div class="container">
+        <div class="content">
+            <div class="heading text-white">
+                <div class="title">Title Here</div>
+                <div class="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br> Vivamus lacinia odio vitae vestibulum
+                    vestibulum.
+                </div>
+            </div>
+            <div class="about-items">
+                <div class="row">
+                    <div class="col-lg-4 col-md-6">
+                        <div class="item text-center text-white mb-4 mb-lg-0">
+                            <div class="img">
+                                <img src="./assets/images/about-1.png" alt="">
+                            </div>
+                            <div class="title">Pet Care</div>
+                            <div class="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="item text-center text-white mb-4 mb-lg-0">
+                            <div class="img">
+                                <img src="./assets/images/about-2.png" alt="">
+                            </div>
+                            <div class="title">Pet Shop</div>
+                            <div class="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="item text-center text-white mb-4 mb-lg-0">
+                            <div class="img">
+                                <img src="./assets/images/about-3.png" alt="">
+                            </div>
+                            <div class="title">Pet Help</div>
+                            <div class="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="left-img">
+        <img src="./assets/images/about-shape-left.png" alt="">
+    </div>
+    <div class="right-img">
+        <img src="./assets/images/about-shape-right.png" alt="">
+    </div>
+</section>
