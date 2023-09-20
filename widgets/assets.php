f566ac5b9e822f6c9b48803dfc81e3c2213b5507
mahibashardeveloper <mahibashar2023@gmail.com>
2023-09-20 10:45:58 +0600

    update links also

--- widgets/assets.php
+++ widgets/assets.php
@@ -2,6 +2,4 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css"/>
-<link rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.11.1/font/bootstrap-icons.min.css"/>
 <link rel="stylesheet" href="assets/css/style.css">
